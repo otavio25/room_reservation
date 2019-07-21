862a31bf4bddc3cfeb90372cd3163b20621814d0
otavio25 <souzaotavio721@gmail.com>
2019-07-20 21:31:34 -0300

    Alterando login do admin

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,6 +14,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 #app/assets
 Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css )
+Rails.application.config.assets.precompile += %w( admin_devise.js admin_devise.css )
 Rails.application.config.assets.precompile += %w( users_backoffice.js users_backoffice.css )
 Rails.application.config.assets.precompile += %w( site.js site.css )
 
